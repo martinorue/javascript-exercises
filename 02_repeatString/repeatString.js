8f7bb2cb54fd6d6d91f2944a47f67089311c1629
martinorue <martin.orue008@gmail.com>
2023-03-11 18:47:44 -0300

    solved exercises from 1 to 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (text, number) {
+    if (number < 0) return 'ERROR';
+    let ret = '';
+    for (let i = 0; i < number; i++) {
+        ret += text;
+    }
+    return ret;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function () {
+    const arr = arguments[0];
+    const args = [...arguments];
+    const numsDelete = args.slice(1);
+    const ret = arr.filter(item => !numsDelete.includes(item));
+    return ret;
 };
 
 // Do not edit below this line
